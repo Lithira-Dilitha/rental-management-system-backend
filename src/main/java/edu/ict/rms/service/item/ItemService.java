7742cc5c88c49a2eda919b419c7824a5f632ebe0
Lithira-Dilitha <lithiradilitha2022@gmail.com>
2024-07-26 14:09:20 +0530

    implemented Item Crud Operations

--- src/main/java/edu/ict/rms/service/item/ItemService.java
+++ src/main/java/edu/ict/rms/service/item/ItemService.java
@@ -1,4 +1,12 @@
 package edu.ict.rms.service.item;
 
+import edu.ict.rms.dto.Item;
+
+import java.util.List;
+
 public interface ItemService {
+    void save(Item item);
+    void update(Item item);
+    void delete(Long id);
+    List<Item>getAllItems();
 }
--- src/main/java/edu/ict/rms/service/item/impl/ItemServiceImpl.java
+++ src/main/java/edu/ict/rms/service/item/impl/ItemServiceImpl.java
@@ -1,6 +1,41 @@
 package edu.ict.rms.service.item.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ict.rms.dto.Item;
+import edu.ict.rms.entity.ItemEntity;
+import edu.ict.rms.repository.ItemRepository;
 import edu.ict.rms.service.item.ItemService;
+import lombok.RequiredArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
+    final ObjectMapper mapper;
+    final ItemRepository repository;
+    @Override
+    public void save(Item item) {
+        repository.save(mapper.convertValue(item, ItemEntity.class));
+    }
+
+    @Override
+    public void update(Item item) {
+        repository.save(mapper.convertValue(item, ItemEntity.class));
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public List<Item> getAllItems() {
+        List<Item>itemList= new ArrayList<>();
+        List<ItemEntity> itemEntities = repository.findAll();
+        itemEntities.forEach(itemEntity ->
+                itemList.add(mapper.convertValue(itemEntity,Item.class)
+                ));
+        return itemList;
+    }
 }
